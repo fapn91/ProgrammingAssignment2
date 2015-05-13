5b9a93dd66107abd454afc5e2ae96c99b2f3a55c
Felipe <felipepuga91@gmail.com>
2015-05-13 16:40:48 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix and cacheSolve creates a matrix and find its inverse. Both functions assume that the matrix is always 
+## a square matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
